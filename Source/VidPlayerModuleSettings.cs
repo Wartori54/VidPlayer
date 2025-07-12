d49d82fb5892f5aa313695e0c98f93259ee2b49c
wartori <wartori54@gmail.com>
2025-07-12 18:38:13 +0200

    Allow disabling the theorafile patch, do not use real time to check if we are kept up and provide original VideoPlayer.cs source

--- Source/VidPlayerModuleSettings.cs
+++ Source/VidPlayerModuleSettings.cs
@@ -2,4 +2,7 @@ namespace Celeste.Mod.VidPlayer;
 
 public class VidPlayerModuleSettings : EverestModuleSettings {
 
+    [SettingName("MODOPTIONS_VIDPLAYER_DISABLEFNAPATCH")]
+    [SettingSubText("MODOPTIONS_VIDPLAYER_DISABLEFNAPATCH_DESC")]
+    public bool DisableFNAPatch { get; set; } = false;
 }
